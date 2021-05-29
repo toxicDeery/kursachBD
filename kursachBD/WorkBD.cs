4f0be443fbdd55ae0af2976a8f9ba92111457970
dolboeb <yadykin_as@office.osu.ru>
2021-05-29 12:13:43 +0500

    Добавьте файлы проекта.

---
+++ kursachBD/WorkBD.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace kursachBD
+{
+    class WorkBD
+    {
+        string Credentials = string.Empty;
+        SqlConnection connection;
+        public WorkBD(string Credentials)
+        {
+            this.Credentials = Credentials;
+            connection = new SqlConnection(Credentials);
+            connection.Open(); GC.SuppressFinalize(this);
+        }
+        ~WorkBD()
+        {
+            connection.Close();
+        }
+    }
+}
