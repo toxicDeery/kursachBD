41105a43d351b58939503f29e5872c65b0f01c29
dolboeb <yadykin_as@office.osu.ru>
2021-05-30 14:09:21 +0500

    рубрика

--- kursachBD/DWork.cs
+++ kursachBD/DWork.cs
@@ -25,11 +25,11 @@ namespace kursachBD
             sqlData.Fill(Temp);
             return Temp;
         }
-        public string addOrganization(string Name)
+        public string addOrganization(string Name, string kratName, string phoneNum, string adress, string email, string site)
         {
             try
             {
-                SqlCommand command = new SqlCommand($"", connection);
+                SqlCommand command = new SqlCommand($"INSERT INTO Организация (Название, Краткое_название, Контактный_телефон, Адрес, Эл_адрес, Адрес_сайта) VALUES('{Name}', '{kratName}', '{phoneNum}',' {adress}', '{email}', '{site}')", connection);
                 return $"Команда выполнена. Задействовано строк таблицы: { command.ExecuteNonQuery()}";
             }
             catch (Exception e)
@@ -41,7 +41,55 @@ namespace kursachBD
         {
             try
             {
-                SqlCommand command = new SqlCommand($"", connection);
+                SqlCommand command = new SqlCommand($"DELETE FROM Организация WHERE Код = {ID}", connection);
+                return $"Команда выполнена. Задействовано строк таблицы: {command.ExecuteNonQuery()}";
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+        }
+        public string editOrganization(string edit, string edit1, string edit2, string edit3, string edit4, string edit5 ,int ID)
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand($"UPDATE Организация SET Название = '{edit}', Краткое_название = '{edit1}', Контактный_телефон = '{edit2}', Адрес = '{edit3}', Эл_адрес = '{edit4}' , Адрес_сайта = '{edit5}' WHERE Код = {ID}");
+                return $"Команда выполнена. Задействовано строк таблицы: {command.ExecuteNonQuery()}";
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+        }
+        public string addRubrick(string Rubrick)
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand($"INSERT INTO Рубрика (Рубрика) VALUES ('{Rubrick}')", connection);
+                return $"Команда выполнена. Задействовано строк таблицы: {command.ExecuteNonQuery()}";
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+        }
+        public string delRubrick(int ID)
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand($"DELETE FROM Рубрика WHERE Код = {ID}", connection);
+                return $"Команда выполнена. Задействовано строк таблицы: {command.ExecuteNonQuery()}";
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+        }
+        public string editRubrick(string edit, int ID)
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand($"UPDATE Рубрика SET Рубрика = '{edit}' WHERE Код = {ID};", connection);
                 return $"Команда выполнена. Задействовано строк таблицы: {command.ExecuteNonQuery()}";
             }
             catch (Exception e)
